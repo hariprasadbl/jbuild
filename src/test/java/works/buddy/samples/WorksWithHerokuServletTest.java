2c4dae9ce96643e9087681a0dea73755c8ac5f16
abhiaggowda <77280124+abhiaggowda@users.noreply.github.com>
2021-04-18 00:40:39 +0530

    new commit
    new commit

--- src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
+++ src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
@@ -36,6 +36,6 @@ public class WorksWithHerokuServletTest {
         when(response.getWriter()).thenReturn(writer);
 
         servlet.doGet(request, response);
-        assertEquals("DEPLOYING USING JENKINS webhoo", new String( out.toByteArray(), "UTF-8"));
+        assertEquals("DEPLOYING USING DOCKER FILE-ABHI", new String( out.toByteArray(), "UTF-8"));
     }
 }
