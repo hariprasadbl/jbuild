0ff46e0f03f1e92a0ad6f0c726722ca465b27928
ABC <ABC@gmail.com>
2022-01-01 08:11:59 +0000

    H

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -13,7 +13,7 @@ public class WorksWithHerokuServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setStatus(404);
         PrintWriter writer = response.getWriter();
-        writer.print("HAPPY NEW YEAR-hari");
+        writer.print("HAPPY NEW YEAR-H");
         writer.close();
     }
 }
